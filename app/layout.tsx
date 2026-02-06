6cb0bec41bc994bfc23a53cbe7b0ad99acd1309a
HadsonRamalho <hadsonramalho@gmail.com>
2026-02-06 17:05:03 -0300

    fix: testando correção de busca

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import { RootProvider } from "fumadocs-ui/provider/next";
 import { env } from "@/lib/env";
 import "katex/dist/katex.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./global.css";
 import Script from "next/script";
-import SearchDialog from "@/components/search";
+import { Provider } from "./search-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,7 +25,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider search={{ SearchDialog }}>{children}</RootProvider>
+        <Provider>{children}</Provider>
       </body>
       <Script src="https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js" />
       <Script src="https://unpkg.com/@babel/standalone/babel.min.js" />
